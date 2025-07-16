b159b53e2a42cc699de35f176854b544403a3c67
lucasreis <lucas.graciott@hotmail.com>
2025-07-16 19:43:33 -0300

    feat: add static database for product categories and related products

---
+++ src/presentation/utils/static-db.ts
@@ -0,0 +1,222 @@
+export const Categories = [
+  {
+    title: "Tecnologia e Gadgets",
+    description:
+      "Acessórios para celular, smartwatches, suportes, carregadores.",
+  },
+  {
+    title: "Fitness e Esportes",
+    description: "Equipamentos leves, elásticos, garrafas, roupas esportivas.",
+  },
+  {
+    title: "Leitura e Escrita",
+    description: "Livros, cadernos, planners, canetas criativas.",
+  },
+  {
+    title: "Cozinha e Gastronomia",
+    description: "Utensílios, formas criativas, organizadores, facas.",
+  },
+  {
+    title: "Decoração e DIY",
+    description: "Luminárias, quadros, itens artesanais.",
+  },
+  {
+    title: "Pets",
+    description: "Brinquedos, coleiras, comedouros, acessórios divertidos.",
+  },
+  {
+    title: "Moda e Estilo",
+    description: "Roupas, bolsas, bijuterias, óculos.",
+  },
+  {
+    title: "Beleza e Cuidados Pessoais",
+    description: "Skincare, pincéis de maquiagem, espelhos com LED.",
+  },
+  {
+    title: "Gaming e Cultura Geek",
+    description: "Acessórios gamers, camisetas nerds, action figures.",
+  },
+  {
+    title: "Viagens e Aventura",
+    description: "Mochilas, organizadores de mala, garrafas térmicas.",
+  },
+  {
+    title: "Música e Instrumentos",
+    description: "Fones de ouvido, acessórios para violão, decoração musical.",
+  },
+  {
+    title: "Fotografia e Vídeo",
+    description: "Tripés, ring lights, capas de câmera, mini estúdios.",
+  },
+  {
+    title: "Natureza e Jardinagem",
+    description: "Vasos criativos, kits de cultivo, ferramentas pequenas.",
+  },
+  {
+    title: "Carros e Motos",
+    description: "Suportes, adesivos, organizadores de porta-malas.",
+  },
+  {
+    title: "Organização e Papelaria Criativa",
+    description: "Pastas, etiquetas, planners decorados.",
+  },
+  {
+    title: "Cultura Pop e Filmes",
+    description: "Produtos de séries, filmes, camisetas temáticas.",
+  },
+  {
+    title: "Espiritualidade e Bem-estar",
+    description: "Incensos, cristais, velas aromáticas, livros de autoajuda.",
+  },
+  {
+    title: "Café e Chás",
+    description: "Canecas criativas, coadores, acessórios temáticos.",
+  },
+  {
+    title: "Pais e Bebês",
+    description: "Brinquedos educativos, babadores, itens para o dia a dia.",
+  },
+  {
+    title: "Sustentabilidade e Eco-friendly",
+    description: "Canudos reutilizáveis, ecobags, copos sustentáveis.",
+  },
+  {
+    title: "Brinquedos e Jogos",
+    description: "Jogos de tabuleiro, quebra-cabeças, brinquedos educativos.",
+  },
+  {
+    title: "Saúde e Bem-estar",
+    description:
+      "Suplementos, equipamentos de ginástica, itens de relaxamento.",
+  },
+];
+
+export const TechAndGadgetsProducts = [
+  {
+    url: "https://www.amazon.com.br/fire-tv-stick-hd/dp/B0CQMT33WX?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=A1ZZFT5FULY4LN&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/JBL-Caixa-Bluetooth-JBLGOESBLK-Pequeno/dp/B09NCFVNK9?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=A1ZZFT5FULY4LN&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.mercadolivre.com.br/soundbar-gamer-redragon-adiemus-rgb-gs560-preto/p/MLB19086162?matt_tool=18956390&pdp_filters=item_id%3AMLB5401881250&from=gshop&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.mercadolivre.com.br/galaxy-smarttag2-pacote-unitario-cor-preto/p/MLB27932163?matt_tool=18956390&pdp_filters=item_id%3AMLB4090898933&from=gshop&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://shopee.com.br/product/883941882/22593099948?gads_t_sig=VTJGc2RHVmtYMTlxTFVSVVRrdENkVHQ3ZkZSUTMrR3pBWmZZNzdrcnRBMlMxVDYyTi85aEpOTng5UDVyalNpbkJJa2xtcEp6Zm91bCt5R0Z2L3A4Y09lL0hESFF6enJMbVdMKytZUnJVRm8wWUhCNGtncHFETFgzTGpSZmttRDIxQVEwcVdkOW1Bb0E1KzVHYWJHUnV3PT0&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://produto.mercadolivre.com.br/MLB-4015543713-sensor-de-temperatura-e-umidade-wifi-tuya-hygrometer-thermo-_JM?matt_tool=18956390&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://shopee.com.br/product/1321572866/18098133121?gads_t_sig=VTJGc2RHVmtYMTlxTFVSVVRrdENkWHlFU0hvQlZFVENpb1FnT09uNDlDSURjbkI5VWw0Y2NsWjR6cTMyNENvK1ZPS0dMWDlielBORldMTFlOTXl0aTRmc1BLT3VzUlFMUm5CQVh1VkFHa2V2VnJRWmRBVnR3WUVyUjBaOU9rUmNvRVM2Y1ovOEhCeC9JRXU1WEkwUmV3PT0&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/Hub-Portas-Deck-Preto-Streamplify/dp/B0BZMKVQJN?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=AX187GNXFU0O3&utm_source=chatgpt.com",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/Echo-Pop-Cor-Preta/dp/B09WXVH7WK/ref=pd_rhf_se_s_pd_crcd_d_sccl_2_1/139-4415017-1785553?pd_rd_w=zhEDw&content-id=amzn1.sym.a6bf656f-dec5-4bf8-a331-f2631d86c1a8&pf_rd_p=a6bf656f-dec5-4bf8-a331-f2631d86c1a8&pf_rd_r=JSK9CZCQ2N5A62BP35Z7&pd_rd_wg=WtXXt&pd_rd_r=b6c40b1c-5d2b-4917-8cfc-cdb397b497d0&pd_rd_i=B09WXVH7WK&th=1",
+    categories: ["Tecnologia e Gadgets"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/Organizador-Tecnologia-Para-Bagagem-Acess%C3%B3rios/dp/B0FBGZTXNW/ref=sr_1_81?dib=eyJ2IjoiMSJ9.DSEjHBYve8mPu3NKBX-ZjKTtb5KqBaH9tV6q3S1Ct8fD3PsSMPzagJFL5SvkZ5Adhs2QCJ_UqMIe023fGAGcI2aUQatvGQ0jN8o3CBe3W-qfDI89Hy_j3FwfsFt1J2zv30QX_ByXgByGGuoi08v5cNO-jkbkfDx0Rs_W_qwvI-uR7QpIZQ77eyJE9u8aik12OhUkjJo3ekqWtR-XzhRVWjcp3gYYifZQTPB5_qTxUgVIFPL8CEbygM6mcAkzetsLR0ZUPaiSb7YfvlFgQIuOWVJIb4_TlP83D1axkDG3Stw.6aqO15AtOaLlMHIcFbAPS8EGNn3jnjQkXw2IiMFnJOE&dib_tag=se&keywords=Tecnologia+e+Gadgets&qid=1752702763&sr=8-81&ufe=app_do%3Aamzn1.fos.6a09f7ec-d911-4889-ad70-de8dd83c8a74&xpid=BD7N4Sfc-25lu",
+    categories: [
+      "Tecnologia e Gadgets",
+      "Organização e Papelaria Criativa",
+      "Viagens e Aventura",
+    ],
+    gender: ["female", "male"],
+  },
+];
+
+export const FitnessAndSportsProducts = [
+  {
+    url: "https://www.amazon.com.br/sspa/click?ie=UTF8&spc=MToyMjI3MzMxNzI0MTQxNTUxOjE3NTI3MDM5NDU6c3BfbXRmOjMwMDQ3NzA3NTI4OTgwMjo6MDo6&url=%2FCompleto-El%25C3%25A1sticas-Exerc%25C3%25ADcios-Muscula%25C3%25A7%25C3%25A3o-Funcional%2Fdp%2FB0DCT4CWZD%2Fref%3Dsr_1_23_sspa%3Fdib%3DeyJ2IjoiMSJ9.IqmUxALpmHMEbjOzlQZmzoPrqMdnTtcxpopJRLDiUagTaoFb5N4b5u9Grw_YVb_wvd0ojHR8Ynd8zisxM249kaObqUOLwoqeCd0Ijh7IoiqPwws-VqW1FIsofhgopRx5z48wFYbL061YhFz3A3DrxfTbECOxmL14I-GSYvjkG7aynwlMYortlvlPVNyNw1AD77XA68y76PXoNdtmoviSGzE24XVGFSEmhV9nZ6daIDc877VxvEpHVuUAVZn9a8rSMjUju2cbf5RW812nIFhgnERN5hR4Su-EEG0FhW6xmdk.FKzJXwZ8fOhIJDBFmYjOu1BHdEakZXNfDeBhRbfBvGg%26dib_tag%3Dse%26keywords%3Dfitness%2Be%2Bsportes%26qid%3D1752703945%26sr%3D8-23-spons%26ufe%3Dapp_do%253Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9tdGY%26psc%3D1",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/Acte-Exerc%C3%ADcios-Funcionais-Fitness-T406/dp/B0F7B23498/ref=sr_1_35?dib=eyJ2IjoiMSJ9.IqmUxALpmHMEbjOzlQZmzoPrqMdnTtcxpopJRLDiUagTaoFb5N4b5u9Grw_YVb_wvd0ojHR8Ynd8zisxM249kaObqUOLwoqeCd0Ijh7IoiqPwws-VqW1FIsofhgopRx5z48wFYbL061YhFz3A3DrxfTbECOxmL14I-GSYvjkG7aynwlMYortlvlPVNyNw1AD77XA68y76PXoNdtmoviSGzE24XVGFSEmhV9nZ6daIDc877VxvEpHVuUAVZn9a8rSMjUju2cbf5RW812nIFhgnERN5hR4Su-EEG0FhW6xmdk.FKzJXwZ8fOhIJDBFmYjOu1BHdEakZXNfDeBhRbfBvGg&dib_tag=se&keywords=fitness+e+sportes&qid=1752703945&sr=8-35&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/Acte-Extensor-Exerc%C3%ADcios-Funcionais-T408/dp/B0F79VCM2H/ref=sr_1_45?dib=eyJ2IjoiMSJ9.IqmUxALpmHMEbjOzlQZmzoPrqMdnTtcxpopJRLDiUagTaoFb5N4b5u9Grw_YVb_wvd0ojHR8Ynd8zisxM249kaObqUOLwoqeCd0Ijh7IoiqPwws-VqW1FIsofhgopRx5z48wFYbL061YhFz3A3DrxfTbECOxmL14I-GSYvjkG7aynwlMYortlvlPVNyNw1AD77XA68y76PXoNdtmoviSGzE24XVGFSEmhV9nZ6daIDc877VxvEpHVuUAVZn9a8rSMjUju2cbf5RW812nIFhgnERN5hR4Su-EEG0FhW6xmdk.FKzJXwZ8fOhIJDBFmYjOu1BHdEakZXNfDeBhRbfBvGg&dib_tag=se&keywords=fitness+e+sportes&qid=1752703945&sr=8-45&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/sspa/click?ie=UTF8&spc=MTozNzI0NTE4MjA4NzE1ODE5OjE3NTI3MDQyNTI6c3BfYXRmX25leHQ6MzAwNzMwMzAwNTMwODAyOjowOjo&url=%2FStep-Academia-Premium-Antiderrapante-Agachamento%2Fdp%2FB0F3F5F5BJ%2Fref%3Dsr_1_52_sspa%3Fdib%3DeyJ2IjoiMSJ9.0X46ldRjZZttjNvnodQxkAhzqNiqv5wH3VS6jtcAvJn-byncCseNlv2D8UCpJn-1xclPnBqzTawOuDF51QX2YFgaoIpZHBLPqVwnJzIGWP8.NgkwJx0Jv722hJYWXDDdxsz46_0VbztfhlIsb2yhI8Q%26dib_tag%3Dse%26keywords%3Dfitness%2Be%2Bsportes%26qid%3D1752704252%26sr%3D8-52-spons%26ufe%3Dapp_do%253Aamzn1.fos.fcd6d665-32ba-4479-9f21-b774e276a678%26xpid%3D2AvZnytoHvzqQ%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9hdGZfbmV4dA%26psc%3D1",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.amazon.com.br/sspa/click?ie=UTF8&spc=MTozNzI0NTE4MjA4NzE1ODE5OjE3NTI3MDQyNTI6c3BfbXRmOjMwMDEzMTQxNDAzMjEwMjo6MDo6&url=%2Fshorts-bermuda-academia-esportes-prote%25C3%25A7ao%2Fdp%2FB0C86P8YV8%2Fref%3Dsr_1_59_sspa%3Fdib%3DeyJ2IjoiMSJ9.z5I1lZfoGFRukGI6RV-K-eP5oxLi6b2ytZzbSVZTyj425k6bIEe-CWh8q9rFUQg2Dr9JNNhkDzjENXPk6_uRMr6BdOvHu6BV06knp-Ek0go6y_ZmvQXdObNAiRJNjkJ_h0nOHdKYahYjyaECksghBxhuEglvP3wv41zUb-H3frqx1FT19r5u6Xp1WiuKsS1ZUJZUs9a6vo8d2MSgwaQ7BvlH8kjYvOdf7VpLE-NYxVR0_z-zCDX0ZCBZo2b50FoV84nx73ZxkQV66LE8ITzihVZfPsXkYNjzOZblhZNuBno.sKXxnBVSN9O_LPsFojxZhTCbSQpdXmzUXB4xzWtFVnA%26dib_tag%3Dse%26keywords%3Dfitness%2Be%2Bsportes%26qid%3D1752704252%26sr%3D8-59-spons%26ufe%3Dapp_do%253Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9%26xpid%3D2AvZnytoHvzqQ%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9tdGY%26psc%3D1",
+    categories: ["Fitness e Esportes"],
+    gender: ["male"],
+  },
+  {
+    url: "https://shopee.com.br/Kit-4-Camiseta-Dry-Masculina-Manga-Curta-Fitness-Academia-Corrida-CrossFit-Poliester-Premium-i.1035529983.19798012231?sp_atk=bb7e1f4c-4a27-4eb9-832d-e24a602c0926&xptdk=bb7e1f4c-4a27-4eb9-832d-e24a602c0926",
+    categories: ["Fitness e Esportes"],
+    gender: ["male"],
+  },
+  {
+    url: "https://shopee.com.br/Conjunto-Fitness-Academia-Preto(top-legging-regata-Nadador)-i.404889078.6982187545?sp_atk=e401654a-f97c-4de7-8c1d-05ca9fe49571&xptdk=e401654a-f97c-4de7-8c1d-05ca9fe49571",
+    categories: ["Fitness e Esportes"],
+    gender: ["female"],
+  },
+  {
+    url: "https://shopee.com.br/Conjunto-Fitness-Roupas-De-Academia-Gin%C3%A1stica-Muscula%C3%A7%C3%A3o-i.386242150.4979430430?sp_atk=34c4c10b-9c31-4004-9fca-1cf31f68e256&xptdk=34c4c10b-9c31-4004-9fca-1cf31f68e256",
+    categories: ["Fitness e Esportes"],
+    gender: ["female"],
+  },
+  {
+    url: "https://shopee.com.br/Strap-Academia-Par-Muscula%C3%A7%C3%A3o-Treino-Muscula%C3%A7%C3%A3o-Strep-Muscula%C3%A7%C3%A3o-i.324999609.15001840274?sp_atk=b3375ba9-93e3-4dcc-8cf3-dabc7b481358&xptdk=b3375ba9-93e3-4dcc-8cf3-dabc7b481358",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://shopee.com.br/Kit-Treino-Em-Casa-Colchonete-Par-Halter-Sextavado-1-kg-Par-Caneleira-1-Kg-Academia-Cross-training-em-Casa-i.624538925.23203404624?sp_atk=e8d6122e-91d8-41f4-9595-9e3168c21ec2&xptdk=e8d6122e-91d8-41f4-9595-9e3168c21ec2",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.decathlon.com.br/bola-de-campo-penalty-bravo-xxv-laranja-productid-2145774907/p?skuId=908381",
+    categories: ["Fitness e Esportes", "Brinquedos e Jogos"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.netshoes.com.br/p/garrafa-de-agua-adidas-performance-bottle-500ml-NQQ-3881-002",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://www.netshoes.com.br/p/garrafa-termica-mormaii-650ml-aco-inox-com-filtro-hermetica-I6F-9568-198",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+  {
+    url: "https://shopee.com.br/kit-3-Garrafa-2000ml-de-%C3%81gua-com-Adesivos-3D-Motivacional-em-ingl%C3%AAs-Squeeze-Capacidade-i.409173014.22496704981?sp_atk=07c120f4-fef0-4911-b0d4-1ed87c89ee3b&xptdk=07c120f4-fef0-4911-b0d4-1ed87c89ee3b",
+    categories: ["Fitness e Esportes"],
+    gender: ["female", "male"],
+  },
+];
